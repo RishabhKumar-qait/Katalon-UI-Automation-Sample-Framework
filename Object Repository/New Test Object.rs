03eda07d89a95bd22bb8237752b2b739aaeccda4
Rishabh Kumar <rishabh.kumar@QAIT.COM>
2019-08-23 10:41:01 +0530

    added new Object Repository

---
+++ Object Repository/New Test Object.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>ea17b0d5-177b-4e89-a031-edd0c6f134a8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>btn_login</name>
+      <type>Main</type>
+      <value>Login</value>
+   </webElementProperties>
+</WebElementEntity>
